a80191da89d7938ea385e9d9a0937ad32a71911f
Andy <andy-nguyen@live.ca>
2021-10-26 12:37:17 -0700

    Completed demo

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,13 +1,12 @@
 // Your web app's Firebase configuration
-var  firebaseConfig = {
-    apiKey: ,
-    authDomain: ",
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId: 
-  };
-  
+var firebaseConfig = {
+  apiKey: "AIzaSyBffDsBfqqnrBnye0R3QEUgnv33nQ4xy24",
+  authDomain: "fir-comp1800-5015b.firebaseapp.com",
+  projectId: "fir-comp1800-5015b",
+  storageBucket: "fir-comp1800-5015b.appspot.com",
+  messagingSenderId: "351790299954",
+  appId: "1:351790299954:web:47675fcb93a403828a752b"
+};
 
-  const app = firebase.initializeApp(firebaseConfig);
-  
+
+const app = firebase.initializeApp(firebaseConfig);
